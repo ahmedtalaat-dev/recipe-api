a100ebd4d2a85f6eab54d000a60fa317731343b7
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-09-22 10:27:29 +0300

    feat: configure recipe API and image uploads

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 require('dotenv').config()
+const connectDB = require("./config/connectDB")
+const recipeRouter = require("./routes/recipeRouter")
 const app = express();
 
 const PORT = process.env.PORT || 3000
+connectDB();
 
-app.listen(3000, () => {
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/recipe", recipeRouter)
+
+app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`)
 })
