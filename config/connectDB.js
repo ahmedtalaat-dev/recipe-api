0816dcd3c2f40dd9b993f4a7ccb2cda34111783c
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-09-22 10:22:07 +0300

    feat: add MongoDB database connection

---
+++ config/connectDB.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+module.exports = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection failed:", error);
+    });
+};
