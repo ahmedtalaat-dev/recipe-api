7e9ebaab9574072ce328138ab82a5d56462adbcc
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-09-23 09:43:53 +0300

    feat: add user authentication routes to app

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
-require('dotenv').config()
-const connectDB = require("./config/connectDB")
-const recipeRouter = require("./routes/recipeRouter")
+require("dotenv").config();
+
+const connectDB = require("./config/connectDB");
+const recipeRouter = require("./routes/recipeRouter");
+const userRouter = require("./routes/userRouter");
+
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
 connectDB();
 
 app.use(express.json());
+
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api/recipe", recipeRouter)
+app.use("/api/recipe", recipeRouter);
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
-})
+  console.log(`App is listening on port ${PORT}`);
+});
