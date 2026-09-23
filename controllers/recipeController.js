263f4e1febba9398261bc7c5281f727a80cccbb5
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-09-23 09:53:55 +0300

    feat: enforce recipe ownership on update

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -43,38 +43,43 @@ const createRecipe = async (req, res) => {
 // Update a recipe
 const updateRecipe = async (req, res) => {
   try {
-    const updateData = {
-      ...req.body,
-    };
-
-    if (req.file) {
-      updateData.image = `/uploads/${req.file.filename}`;
-    }
-
-    const recipe = await Recipe.findByIdAndUpdate(req.params.id, updateData, {
-      new: true,
-      runValidators: true,
-    });
-
+    const recipe = await Recipe.findById(req.params.id);
     if (!recipe) {
       return res.status(404).json({ message: "Recipe not found" });
     }
-
-    res.status(200).json(recipe);
+    if (recipe.createdBy.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to update this recipe" });
+    }
+    const updateData = { ...req.body };
+    if (req.file) {
+      updateData.image = `/uploads/${req.file.filename}`;
+    }
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true, runValidators: true },
+    );
+    res.status(200).json(updatedRecipe);
   } catch (error) {
-    res.status(400).json({ message: "Failed to update recipe" });
+    res.status(500).json({ message: "Failed to update recipe" });
   }
 };
 
 // Delete a recipe
 const deleteRecipe = async (req, res) => {
   try {
-    const recipe = await Recipe.findByIdAndDelete(req.params.id);
-
+    const recipe = await Recipe.findById(req.params.id);
     if (!recipe) {
       return res.status(404).json({ message: "Recipe not found" });
     }
-
+    if (recipe.createdBy.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to delete this recipe" });
+    }
+    await recipe.deleteOne();
     res.status(200).json({ message: "Recipe deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Failed to delete recipe" });
